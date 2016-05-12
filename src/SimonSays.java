ce140d4767a5ffed456aa6481b3d67b0af544459
TJHL <league@WTS5.attlocal.net>
2016-05-11 18:23:14 -0700

    arrow
    jeopardy

--- src/SimonSays.java
+++ src/SimonSays.java
@@ -16,7 +16,9 @@ public class SimonSays extends KeyAdapter {
  // Complete steps 1 - 7 before you test
  // 1. Make a JFrame variable 
 	JFrame a;	
-	int x = new Random().nextInt(2);
+	int x ;
+	int score = 0;
+	
  	HashMap<Integer,String> images = new HashMap<Integer, String>();
  	private int imageIndex;
  	private int tries = 0;
@@ -25,28 +27,38 @@ public class SimonSays extends KeyAdapter {
  	private  void makeAlbum() {
  // 2. add 4 images which match keyboard keys like this: images.put(new Integer(KeyEvent.VK_UP), "image.jpg");
  		images.put(new Integer(KeyEvent.VK_UP), "up.png");
- 		images.put(new Integer(KeyEvent.VK_K), "k.jpeg");
- 		images.put(new Integer(KeyEvent.VK_F), "f.png");
- 		images.put(new Integer(KeyEvent.VK_C), "c.png");
+ 		images.put(new Integer(KeyEvent.VK_DOWN), "down.png");
+ 		images.put(new Integer(KeyEvent.VK_RIGHT), "right.jpeg");
+ 		images.put(new Integer(KeyEvent.VK_LEFT), "left.png");
  // 3. Tell the user to "Press the matching key when 'Simon says' otherwise press a different key"
-        System.out.println("You must press the matching key when 'Simon Says' otherwise press any other key.");
+        JOptionPane.showMessageDialog(null,"You must press the matching key when 'Simon Says' otherwise press any other key. After 3 wrong answers, you lose!!");
  // 4. call the method to show an image
         showImage();
  	}
 	public void keyPressed(KeyEvent e) {
     	int keyCode = e.getKeyCode();
     	// 16. make a points variable to track the score. tell the user their score at the end.
-    	int score = 0;
+    	
     	//17. if the keyCode matches the imageIndex and "Simon says..."  increase their score
     	if(keyCode == imageIndex){
     		if(x == 1){
+    			speak("wrong");
+    			tries+=1;
+    		}
+    		else{
+    			
     			speak("correct");
-    			score+=1;
+    			score +=1;
     		}
     		
     	}
     	else if(keyCode != imageIndex){
-    		if(x ==2){
+    		if(x ==0){
+    			speak("wrong");
+    			tries+=1;
+    		}
+    		else{
+    			
     			speak("correct");
     			score +=1;
     		}
@@ -56,14 +68,17 @@ public class SimonSays extends KeyAdapter {
     		
     		speak("Wrong");
     	}
-    	int tries = 0;
+    	System.out.println("Your Score is "+score);
+  
     	//18.   if the keyCode doesn't match the imageIndex and "Simon didn't say..."  increase their score	
     	//19. Use the speak method to tell the user if they were correct or not
     	//13. increment tries by 1
-    	tries+=1;
+   
     	//14. if tries is greater than 9 (or however many you want)
-    	if (tries >9){
-    		System.out.println("You Lost!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    	if (tries >2){
+    		JOptionPane.showMessageDialog(null,"You Lost!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    		
+    		JOptionPane.showMessageDialog(null,"Your score is "+ score);
     		System.exit(0);
     	}
     	//15.    	exit the program
@@ -75,29 +90,30 @@ public class SimonSays extends KeyAdapter {
 	}
 	private void showImage() {
     	//5. initialize your frame to a new JFrame()
-		JFrame a = new JFrame();
+		a = new JFrame();
     	//6. set the frame to visible
 		a.setVisible(true);
   	 //frame.add(getNextRandomImage()); //7. rename to the name of your frame
 		a.add(getNextRandomImage());
     	// 8. set the size of the frame 
-     	a.setSize(200, 200);
+     	a.setSize(400, 400);
     	// 9. add a key listener to the frame
      	a.addKeyListener(this);
    	 //10. Use the speak method to either say "Simon says press this key" or "Press this key"
     	//Hint: use the simonSays int and a random number
-     	//int x = new Random().nextInt(2);
+     	 x = new Random().nextInt(2);
      	System.out.println("0");
-     	if(x == 1){
+     	if(x == 0){
      	speak("Simon says press this key!");
      	}
-     	else if(x == 2){
+     	else if(x == 1){
      		
      		speak("Press this key!");
      	}
 	}
 	private Component getNextRandomImage() {
-    	this.imageIndex = new Random().nextInt(4) + 37;
+    	this.imageIndex = new Random().nextInt(4)+37;
+    	System.out.println(this.imageIndex);
     	return loadImage(images.get(imageIndex));
 	}
 	private JLabel loadImage(String fileName) {
